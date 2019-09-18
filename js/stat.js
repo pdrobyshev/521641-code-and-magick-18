24338091bf4723a2d6e09f6ef7011723a8e53891
Pyotr Drobyshev <pa.drobyshev@gmail.com>
2019-09-18 23:47:19 +0600

    правки по ошибкам

--- js/stat.js
+++ js/stat.js
@@ -1,59 +1,83 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
+var cloudParams = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10
+};
+
+var barParams = {
+  WIDTH: 40,
+  HEIGHT: 150,
+  GAP: 50
+};
+
+var colors = {
+  BLACK: '#000000',
+  WHITE: '#ffffff',
+  SHADOW: 'rgba(0, 0, 0, 0.7)',
+  MY_PLAYER: 'rgba(255, 0, 0, 1)',
+  getRandomHsl: function () {
+    return 'hsl(200, ' + Math.floor(Math.random() * 100) + '%,  45%)';
+  }
+};
+
 var GAP = 10;
-var BAR_WIDTH = 40;
-var BAR_HEIGHT = 150;
-var BAR_GAP = 50;
-var OFFSET_X = BAR_WIDTH + BAR_GAP;
-var MIN_OFFSET_X = CLOUD_X + BAR_WIDTH;
+var TEXT_GAP = 30;
+var OFFSET_X = barParams.WIDTH + barParams.GAP;
+var MIN_OFFSET_X = cloudParams.X + barParams.WIDTH;
+var FONT_PARAMS = '16px "PT Mono"';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, cloudParams.WIDTH, cloudParams.HEIGHT);
 };
 
-var renderText = function (ctx) {
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px "PT Mono"';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 5);
+var renderWinnerText = function (ctx) {
+  ctx.fillStyle = colors.BLACK;
+  ctx.font = FONT_PARAMS;
+  /*
+    Как всё же лучше побороть магические числа?
+    Если бы использовал только GAP - пришлось бы умножать на 2, 3 или 5, как было до этого
+    Ввёл TEXT_GAP - грубо говоря высота текста(хотя меня это уже смущает - высота текста, взятая из воздуха)
+    Вводить абстрактные переменыне вроде TITLE_GAP или любой другой, с разными значениями только ради пары мест в коде - тоже не очень
+  */
+  ctx.fillText('Ура вы победили!', cloudParams.X + GAP * 2, cloudParams.Y + TEXT_GAP);
+  ctx.fillText('Список результатов:', cloudParams.X + GAP * 2, cloudParams.Y + TEXT_GAP + GAP * 2);
 };
 
 var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+  return Math.max.apply(null, arr);
+};
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
+var renderPlayerName = function (ctx, namesArray, i) {
+  ctx.fillText(namesArray[i], MIN_OFFSET_X + OFFSET_X * i, cloudParams.HEIGHT);
+};
 
-  return maxElement;
+var renderPlayerTime = function (ctx, timesArray, i, maxTime) {
+  ctx.fillText(Math.round(timesArray[i]), MIN_OFFSET_X + OFFSET_X * i, (-barParams.HEIGHT * timesArray[i]) / maxTime + cloudParams.X + barParams.HEIGHT - GAP);
+};
+
+var renderPlayerBar = function (ctx, timesArray, i, maxTime) {
+  ctx.fillRect(MIN_OFFSET_X + OFFSET_X * i, cloudParams.X + barParams.HEIGHT, barParams.WIDTH, (-barParams.HEIGHT * timesArray[i]) / maxTime);
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+  renderCloud(ctx, cloudParams.X + GAP, cloudParams.Y + GAP, colors.SHADOW);
+  renderCloud(ctx, cloudParams.X, cloudParams.Y, colors.WHITE);
 
-  renderText(ctx);
+  renderWinnerText(ctx);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], MIN_OFFSET_X + OFFSET_X * i, CLOUD_HEIGHT);
-    ctx.fillText(Math.round(times[i]), MIN_OFFSET_X + OFFSET_X * i, (-BAR_HEIGHT * times[i]) / maxTime + CLOUD_X + BAR_HEIGHT - GAP);
+    ctx.fillStyle = colors.BLACK;
+    renderPlayerName(ctx, names, i);
+    renderPlayerTime(ctx, times, i, maxTime);
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(200, ' + Math.floor(Math.random() * 101) + '%,  45%)';
-    }
+    ctx.fillStyle = names[i] === 'Вы' ? colors.MY_PLAYER : colors.getRandomHsl();
 
-    ctx.fillRect(MIN_OFFSET_X + OFFSET_X * i, CLOUD_X + BAR_HEIGHT, BAR_WIDTH, (-BAR_HEIGHT * times[i]) / maxTime);
+    renderPlayerBar(ctx, times, i, maxTime);
   }
 };
